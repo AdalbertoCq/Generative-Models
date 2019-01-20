c5747f38cab0b511dd9ff105330baf701a1a36c2
Adalberto Claudio Quiros <aclaudioquiros@gmail.com>
2019-01-20 10:57:52 +0000

    Indent fix

--- GANs/WGAN.py
+++ GANs/WGAN.py
@@ -34,70 +34,70 @@ class WGAN:
 	def discriminator(self, images, reuse):
 		with tf.variable_scope('discriminator', reuse=reuse):
 			# Input Shape: (None, 28, 28, 1)
-	        # Padding = 'Same' -> H_new = H_old // Stride
-	        
-	        # Conv layer.
-	        net = tf.layers.conv2d(inputs=images, filters=64, kernel_size=(5,5), strides=(2, 2), padding='same', kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d())
-	        net = leakyReLU(net, alpha)
-	        # Shape: (None, 16, 16, 64)
-	        
-	        # Conv layer.
-	        net = tf.layers.conv2d(inputs=net, filters=128, kernel_size=(5,5), strides=(2, 2), padding='same', kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d())
-	        net = tf.layers.batch_normalization(net, training=True)
-	        net = leakyReLU(net, alpha)
-	        # Shape: (None, 8, 8, 128)
+			# Padding = 'Same' -> H_new = H_old // Stride
 
-	        net = tf.layers.flatten(net, name='flatten')
-	        # Shape: (None, 8*8*128)
+			# Conv layer.
+			net = tf.layers.conv2d(inputs=images, filters=64, kernel_size=(5,5), strides=(2, 2), padding='same', kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d())
+			net = leakyReLU(net, alpha)
+			# Shape: (None, 16, 16, 64)
 
-	        # Dense.
-	        net = tf.layers.dense(net, 1024, activation=None)
-	        net = tf.layers.batch_normalization(net, training=True)
-	        net = leakyReLU(net, alpha)
-	        # Shape: (None, 1024)
-	        
-	        # Dense
-	        logits = tf.layers.dense(net, 1, activation=None)
-	        # Shape: (None, 1)
-	        output = tf.nn.sigmoid(logits)
-        
-        return output, logits
+			# Conv layer.
+			net = tf.layers.conv2d(inputs=net, filters=128, kernel_size=(5,5), strides=(2, 2), padding='same', kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d())
+			net = tf.layers.batch_normalization(net, training=True)
+			net = leakyReLU(net, alpha)
+			# Shape: (None, 8, 8, 128)
+
+			net = tf.layers.flatten(net, name='flatten')
+			# Shape: (None, 8*8*128)
+
+			# Dense.
+			net = tf.layers.dense(net, 1024, activation=None)
+			net = tf.layers.batch_normalization(net, training=True)
+			net = leakyReLU(net, alpha)
+			# Shape: (None, 1024)
+
+			# Dense
+			logits = tf.layers.dense(net, 1, activation=None)
+			# Shape: (None, 1)
+			output = tf.nn.sigmoid(logits)
+
+		return output, logits
 
 	def generator(self, z_input, reuse, is_train):
 		with tf.variable_scope('generator', reuse=reuse):
 			# Dense layer.
-	        net = tf.layers.dense(inputs=z_input, units=1024, activation=None)
-	        net = tf.layers.batch_normalization(net, training=is_train)
-	        net = leakyReLU(net, alpha)
-	        # Shape: (None, 1024)
-	        
-	        # Dense layer.
-	        net = tf.layers.dense(inputs=net, units=128*7*7, activation=None)
-	        net = tf.layers.batch_normalization(net, training=is_train)
-	        net = leakyReLU(net, alpha)
-	        # Shape: (None, 128*7*7)
-	        
-	        net = tf.reshape(net, (-1, 7, 7, 128))
-	        # Shape: (None, 7, 7, 128)
-	        
-	        # Conv
-	        net = tf.layers.conv2d_transpose(inputs=net, filters=128, kernel_size=(2,2), strides=(2,2), padding='same', kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d())
-	        net = tf.layers.batch_normalization(net, training=is_train)
-	        net = leakyReLU(net, alpha)
-	        # Shape: (None, 14, 14, 128)
-	        
-	        # Conv
-	        net = tf.layers.conv2d(inputs=net, filters=128, kernel_size=(5,5), strides=(1,1), padding='same', kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d())
-	        net = tf.layers.batch_normalization(net, training=is_train)
-	        net = leakyReLU(net, alpha)
-	        # Shape: (None, 14, 14, 128)
-	        
-	        # Conv
-	        logits = tf.layers.conv2d_transpose(inputs=net, filters=out_channel_dim, kernel_size=(2,2), strides=(2,2), padding='same', kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d())
-	        # Shape: (None, 28, 28, 1)
-	        output = tf.nn.sigmoid(logits)
-	        
-        return output
+			net = tf.layers.dense(inputs=z_input, units=1024, activation=None)
+			net = tf.layers.batch_normalization(net, training=is_train)
+			net = leakyReLU(net, alpha)
+			# Shape: (None, 1024)
+
+			# Dense layer.
+			net = tf.layers.dense(inputs=net, units=128*7*7, activation=None)
+			net = tf.layers.batch_normalization(net, training=is_train)
+			net = leakyReLU(net, alpha)
+			# Shape: (None, 128*7*7)
+
+			net = tf.reshape(net, (-1, 7, 7, 128))
+			# Shape: (None, 7, 7, 128)
+
+			# Conv
+			net = tf.layers.conv2d_transpose(inputs=net, filters=128, kernel_size=(2,2), strides=(2,2), padding='same', kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d())
+			net = tf.layers.batch_normalization(net, training=is_train)
+			net = leakyReLU(net, alpha)
+			# Shape: (None, 14, 14, 128)
+
+			# Conv
+			net = tf.layers.conv2d(inputs=net, filters=128, kernel_size=(5,5), strides=(1,1), padding='same', kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d())
+			net = tf.layers.batch_normalization(net, training=is_train)
+			net = leakyReLU(net, alpha)
+			# Shape: (None, 14, 14, 128)
+
+			# Conv
+			logits = tf.layers.conv2d_transpose(inputs=net, filters=out_channel_dim, kernel_size=(2,2), strides=(2,2), padding='same', kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d())
+			# Shape: (None, 28, 28, 1)
+			output = tf.nn.sigmoid(logits)
+
+		return output
 
 
 
@@ -131,27 +131,27 @@ class WGAN:
 
 			# Weight Clipping on Discriminator, this is done to ensure the Lipschitz constrain.
 			train_discriminator = tf.train.AdamOptimizer(self.learning_rate_input, self.beta_1, self.beta_2).minimize(self.loss_dis, var_list=discriminator_variables)
-	        dis_weight_clipping = [value.assign(tf.clip_by_value(value, -self.clipping, self.clipping)) for value in discriminator_variables]
-	        train_discriminator = tf.group(*[train_discriminator, dis_weight_clipping])
+			dis_weight_clipping = [value.assign(tf.clip_by_value(value, -self.clipping, self.clipping)) for value in discriminator_variables]
+			train_discriminator = tf.group(*[train_discriminator, dis_weight_clipping])
 
 			train_generator = tf.train.AdamOptimizer(self.learning_rate_input, self.beta_1, self.beta_2).minimize(self.loss_gen, var_list=generator_variables)
 
 			'''
 			RMS_optimizer_dis = tf.train.RMSPropOptimizer(learning_rate=learning_rate)
-	        train_discriminator = RMS_optimizer_dis.minimize(loss_dis , var_list=discriminator_variables)
-	        
-	        # Weight Clipping on Discriminator, this is done to ensure the Lipschitz constrain.
-	        dis_weight_clipping = [value.assign(tf.clip_by_value(value, -c, c)) for value in discriminator_variables]
-	        
-	        train_discriminator = tf.group(*[train_discriminator, dis_weight_clipping])
-	        
-	        # Generator.
-	        RMS_optimizer_gen = tf.train.RMSPropOptimizer(learning_rate=learning_rate)
-	        train_generator = RMS_optimizer_gen.minimize(loss_gen, var_list=generator_variables)
+			train_discriminator = RMS_optimizer_dis.minimize(loss_dis , var_list=discriminator_variables)
+			
+			# Weight Clipping on Discriminator, this is done to ensure the Lipschitz constrain.
+			dis_weight_clipping = [value.assign(tf.clip_by_value(value, -c, c)) for value in discriminator_variables]
+			
+			train_discriminator = tf.group(*[train_discriminator, dis_weight_clipping])
+			
+			# Generator.
+			RMS_optimizer_gen = tf.train.RMSPropOptimizer(learning_rate=learning_rate)
+			train_generator = RMS_optimizer_gen.minimize(loss_gen, var_list=generator_variables)
 			'''
 			
 		return train_discriminator, train_generator
-    
+
 
 	def build_model(self):
 		# Inputs.
@@ -180,34 +180,34 @@ class WGAN:
 		img_storage, latent_storage, checkpoints = setup_output(show_epochs, epochs, data, n_images, self.z_dim, data_out_path, self.model_name)
 
 		with tf.Session() as session:
-		    session.run(tf.global_variables_initializer())
-		    for epoch in range(1, epochs+1):
-		        for batch_images, batch_labels in data.training:
-		            # Inputs.
-		            z_batch = np.random.uniform(low=-1., high=1., size=(self.batch_size, self.z_dim))               
-		            feed_dict = {self.z_input:z_batch, self.real_images:batch_images, self.learning_rate_input: self.learning_rate}
+			session.run(tf.global_variables_initializer())
+			for epoch in range(1, epochs+1):
+				for batch_images, batch_labels in data.training:
+					# Inputs.
+					z_batch = np.random.uniform(low=-1., high=1., size=(self.batch_size, self.z_dim))
+					feed_dict = {self.z_input:z_batch, self.real_images:batch_images, self.learning_rate_input: self.learning_rate}
 
-		            # Update critic.
-		            session.run(self.train_discriminator, feed_dict=feed_dict)
+					# Update critic.
+					session.run(self.train_discriminator, feed_dict=feed_dict)
 
-		            # Update generator after n_critic updates from discriminator.
-		            if run_epochs%self.n_critic :
-		            	session.run(self.train_generator, feed_dict=feed_dict)
+					# Update generator after n_critic updates from discriminator.
+					if run_epochs%self.n_critic :
+						session.run(self.train_generator, feed_dict=feed_dict)
 
-		            # Print losses and Generate samples.
-		            if run_epochs % print_epochs == 0:
-		                feed_dict = {self.z_input:z_batch, self.real_images:batch_images}
-		                epoch_loss_dis, epoch_loss_gen = session.run([self.loss_dis, self.loss_gen], feed_dict=feed_dict)
-		                losses.append((epoch_loss_dis, epoch_loss_gen))
-		                print('Epochs %s/%s: Generator Loss: %s. Discriminator Loss: %s' % (epoch, epochs, np.round(epoch_loss_gen, 4), np.round(epoch_loss_dis, 4)))
-		            if run_epochs % show_epochs == 0:
-		                gen_samples, sample_z = show_generated(session=session, z_input=self.z_input, z_dim=self.z_dim, output_fake=self.output_gen, n_images=n_images)
-		                img_storage[run_epochs//show_epochs] = gen_samples
-		                latent_storage[run_epochs//show_epochs] = sample_z
-		                saver.save(sess=session, save_path=checkpoints, global_step=run_epochs)
+					# Print losses and Generate samples.
+					if run_epochs % print_epochs == 0:
+						feed_dict = {self.z_input:z_batch, self.real_images:batch_images}
+						epoch_loss_dis, epoch_loss_gen = session.run([self.loss_dis, self.loss_gen], feed_dict=feed_dict)
+						losses.append((epoch_loss_dis, epoch_loss_gen))
+						print('Epochs %s/%s: Generator Loss: %s. Discriminator Loss: %s' % (epoch, epochs, np.round(epoch_loss_gen, 4), np.round(epoch_loss_dis, 4)))
+					if run_epochs % show_epochs == 0:
+						gen_samples, sample_z = show_generated(session=session, z_input=self.z_input, z_dim=self.z_dim, output_fake=self.output_gen, n_images=n_images)
+						img_storage[run_epochs//show_epochs] = gen_samples
+						latent_storage[run_epochs//show_epochs] = sample_z
+						saver.save(sess=session, save_path=checkpoints, global_step=run_epochs)
 
-		            run_epochs += 1
-		        data.training.reset()
+					run_epochs += 1
+				data.training.reset()
 
 		return losses
 
